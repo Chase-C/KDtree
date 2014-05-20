5730a9921bbd1333cca70b842c6d4d920a24f0e1
Chase Cummings <chaseecummings@gmail.com>
2014-05-19 21:53:00 -0700

    Nearest neighbor search implemented

--- src/KDtree.hs
+++ src/KDtree.hs
@@ -5,7 +5,8 @@ module KDtree where
 import Control.Applicative
 import Data.Bits
 
-import qualified Data.List as L
+import qualified Data.Foldable as F
+import qualified Data.List     as L
 
 import Vec3D
 
@@ -20,6 +21,18 @@ data KDtree a = Node
               | Leaf
                 deriving (Show)
 
+instance F.Foldable KDtree where
+    foldl func i Leaf = i
+    foldl func i (Node (obj, _) _ r l) =
+        let a = F.foldl func i l
+            b = func a obj
+        in  F.foldl func b r
+
+prettyPrint :: (Show a) => KDtree a -> String
+prettyPrint Leaf = "Leaf\n"
+prettyPrint (Node obj axis r l) = "Node {\n\t" ++ "Object: " ++ (show obj) ++ "\n\tAxis: " ++ (show axis) ++
+    "\nLeft -\n" ++ (prettyPrint l) ++ "Right -\n" ++ (prettyPrint r) ++ "\n}\n"
+
 ---------------------------------------------------------
 
 data Axis = X | Y | Z deriving (Show, Eq, Enum, Bounded)
@@ -28,14 +41,10 @@ next :: Axis -> Axis
 next Z = X
 next x = succ x
 
-prev :: Axis -> Axis
-prev X = Z
-prev x = pred x
-
-cood :: Axis -> (Vec3D -> Float)
-cood X = vX
-cood Y = vY
-cood Z = vZ
+coord :: Axis -> (Vec3D -> Float)
+coord X = vX
+coord Y = vY
+coord Z = vZ
 
 ---------------------------------------------------------
 
@@ -51,4 +60,24 @@ insertList' xs axis = Node
                         , kdLeft   = insertList' l $ next axis
                         }
     where sorted   = L.sortBy (\(_, a) (_, b) -> let c = coord axis in compare (c a) (c b)) xs
-          (r, m:l) = L.splitAt (length sorted `div` 2) sorted
+          (l, m:r) = L.splitAt (length sorted `div` 2) sorted
+
+nearestNeighbor :: KDtree a -> Vec3D -> Maybe a
+nearestNeighbor tree pos = fst <$> nearestNeighbor' tree pos
+
+nearestNeighbor' :: KDtree a -> Vec3D -> Maybe (a, Float)
+nearestNeighbor' Leaf _ = Nothing
+nearestNeighbor' (Node (obj, pos) axis l r) pt =
+    case candidate of
+      Just (_, sd) -> if sd > (offset * offset)
+                        then cmpPts candidate $ nearestNeighbor' other pt
+                        else candidate
+      Nothing      -> Nothing
+    where offset       = coord axis pt - coord axis pos
+          (sub, other) = if offset > 0 then (r, l) else (l, r)
+          sqDist       = vSqLen $ vSub pos pt
+          candidate    = cmpPts (Just (obj, sqDist)) (nearestNeighbor' sub pt)
+          cmpPts (Just a) (Just b) = if snd a > snd b then Just b else Just a
+          cmpPts (Just a) _        = Just a
+          cmpPts _        (Just b) = Just b
+          cmpPts _        _        = Nothing
--- src/Main.hs
+++ src/Main.hs
@@ -2,33 +2,26 @@ module Main (main) where
 
 --------------------------------------------------------------------------------
 
-import qualified Octree as O
+import Data.Maybe
+import qualified KDtree as KD
 import Vec3D
 
 --------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
-    let a = O.emptyOctree (Vec3D (0, 0, 0)) 16 :: O.Octree Int
-        b = O.insert a (1,  Vec3D (  1,    2,    3))
-        c = O.insert b (2,  Vec3D ((-1), (-2), (-3)))
-        d = O.insert c (3,  Vec3D ((-9),   2,    3))
-        e = O.insert d (4,  Vec3D (  5,  (-2),   5))
-        f = O.insert e (5,  Vec3D (  2,    6,    6))
-        g = O.insert f (18, Vec3D (  1,    2,  (-3)))
-        h = O.insert g (6,  Vec3D ((-5), (-5),   4))
-        i = O.insert h (7,  Vec3D (  5,  (-4), (-5)))
-        j = O.insert i (8,  Vec3D ((-6),   5,  (-5)))
-        k = O.insert j (9,  Vec3D (  5,    6,    5))
-        l = O.insert k (0,  Vec3D ((-5), (-5), (-6)))
-        m = O.insert l (19, Vec3D (2, 2, 2))
-        n = O.insert m (11, Vec3D (2, 2, 6))
-        o = O.insert n (12, Vec3D (6, 6, 6))
-        p = O.insert o (13, Vec3D (6, 2, 2))
-        q = O.insert p (14, Vec3D (2, 6, 2))
-    putStrLn $ O.prettyPrint q
+    let xs = (0, Vec3D ( 1,  1,  1))
+           : (1, Vec3D (-1,  1,  1))
+           : (2, Vec3D ( 1, -1,  1))
+           : (3, Vec3D ( 1,  1, -1))
+           : (4, Vec3D (-1, -1,  1))
+           : (5, Vec3D ( 1, -1, -1))
+           : (6, Vec3D (-1,  1, -1))
+           : (7, Vec3D (-1, -1, -1))
+           : (8, Vec3D ( 5,  1,  1))
+           : (9, Vec3D ( 1,  5, -5))
+           : [] :: [(Int, Vec3D)]
+        tree = KD.insertList xs
 
-    let r = O.splitWith q ((>8) . O.count)
-    putStrLn $ O.prettyPrint r
-    putStrLn $ show $ O.getNearObjects   r (Vec3D (8, 8, 8))
-    putStrLn $ show $ O.getRadiusObjects r (Vec3D (0, 0, 0)) 6
+    putStrLn $ KD.prettyPrint tree
+    putStrLn $ show $ fromMaybe (-1) $ KD.nearestNeighbor tree $ Vec3D (5, 5, 2)
